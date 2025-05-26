682ae5c8249a59cf6f277abb72c42f484a034fc0
Холостова Арина <airsha2004@gmail.com>
2025-05-26 20:03:45 +0300

    Create Project

---
+++ ExamVikings/src/main/java/com/mycompany/examvikings/ExamVikings.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examvikings;
+
+/**
+ *
+ * @author GOSPOGA
+ */
+public class ExamVikings {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
